346b7dedf4c9108230d977ae9579e62c080d7236
Yasuhiro Arakaki <yas.arakaki@gmail.com>
2024-02-14 03:01:22 +0000

    fix

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const expect_column_max_to_be_between = require("./includes/expect_column_max_to_be_between");
-const expect_column_min_to_be_between = require("./includes/expect_column_min_to_be_between");
+const expect_column_max_to_be_between = require("./includes/expect_column_max_to_be_between.js");
+const expect_column_min_to_be_between = require("./includes/expect_column_min_to_be_between.js");
 
-module.exports = (params) => {
+exports.expect_column_max_to_be_between = (params) => {
+  expect_column_max_to_be_between(params);
+}
 
-    params = {
-      // TODO: set default params
-      ...params
-    };
-
-    return {
-      expect_column_max_to_be_between: expect_column_max_to_be_between(params),
-      expect_column_min_to_be_between: expect_column_min_to_be_between(params),
-    };
+exports.expect_column_min_to_be_between = (params) => {
+  expect_column_min_to_be_between(params);
 }
